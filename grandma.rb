f2f732ad424cdc051c4821ae0f23be25492be380
Carden Z <ephemere@protonmail.com>
2018-12-28 23:44:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,10 +3,10 @@
 def speak_to_grandma(words)
 	if words == 'I LOVE YOU GRANDMA!'
 		puts 'I LOVE YOU TOO PUMPKIN!'
-	elsif words == words.upcase
-		puts 'NO, NOT SINCE 1938!'
-	else
-		puts 'HUH?! SPEAK UP, SONNY!'
+	# elsif words == words.upcase
+	# 	puts 'NO, NOT SINCE 1938!'
+	# else
+	# 	puts 'HUH?! SPEAK UP, SONNY!'
 	end
 end
 
